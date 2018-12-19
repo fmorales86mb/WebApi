6c5254d7fa52df5d5895eb75f5122d4ba7084bc6
Federico Morales <federico.morales@neoris.cxnetworks.net>
2018-12-19 14:13:18 -0300

    Get Started
    Proyecto introductorio.

---
+++ 01 - Get Started/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //Para simplificar el ejemplo, los productos se almacenan en una matriz fija dentro de la clase de controlador
+        Product[] products = new Product[]
+       {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+       };
+        /// <summary>
+        /// El GetAllProducts método devuelve la lista completa de productos como un IEnumerable<producto> tipo.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        /// <summary>
+        /// El GetProduct método busca un solo producto por su identificador.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
