0e55de811bbb861c3620e421c382b48c8e358aad
Federico Morales <federico.morales@neoris.cxnetworks.net>
2018-12-20 11:09:57 -0300

    Web API con WebForms.
    Proyecto basico con un WebAPI que sirve a una pagina mostrando lista de
    productos.

---
+++ 03 - WebAPI con WebForms/WebForms/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+namespace WebForms
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Ver luego enrutamiendo en asp.net web api
+            RouteTable.Routes.MapHttpRoute(    
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+        }
+    }
+}
