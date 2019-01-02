570b1a76323b4a2369702ee68369b5954873c598
Federico Morales <federico.morales@neoris.cxnetworks.net>
2019-01-02 14:55:36 -0300

    WebAPI con Entity Framework
    Se agregan Modelos y Controladores. Se crea la DB.

--- 02 - WebAPI 2 con Entity Framework 6/BookService/BookService/Models/Book.cs
+++ 02 - WebAPI 2 con Entity Framework 6/BookService/BookService/Models/Book.cs
@@ -7,7 +7,7 @@ namespace BookService.Models
         public int Id { get; set; }
         [Required]
         public string  Title { get; set; }
-        public int year { get; set; }
+        public int Year { get; set; }
         public decimal Price { get; set; }        
         public string Genre { get; set; }
 
